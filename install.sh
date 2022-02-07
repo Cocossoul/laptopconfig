942bd6c4276b656aeda4c406aed7033c5f6984a4
Corentin Pape <corentin.pape@epita.fr>
2022-02-07 13:26:53 +0100

    feat: add feh and vim

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ sudo pacman -Syyu
 # Deleting /var/lib/pacman/sync/*
 # Readding mirrors sudo pacman-mirrors --continent
 
+# Vim as fast as we can
+sudo pacman -S vim
+
 # Install google-chrome
 sudo pamac build google-chrome
 
@@ -27,11 +30,26 @@ sudo pacman -S redshift
 redshift -O 3500
 
 # i3 tiiiime!
-sudo pacman -S i3-gaps polybar dmenu i3lock
+sudo pacman -S i3-gaps polybar dmenu i3lock feh
 cp config/polybar ~/.config/
 cp config/i3 ~/.config
 cp config/picom.conf ~/.config
 
 # reload i3 config with $mod4+Shift+r
 
+# Change luminosity
+# Straightforward way:
+# sudo vim /sys/class/backlight/intel_backlight/brightness
+sudo pacman -S xorg-xrandr xorg-xbacklight
+sudo usermod -a -G video coco
+# TODO: make it work
+
+# git config
+git config --global user.email "corentin.pape@epita.fr"
+git config --global user.name "Corentin Pape"
+
+# vscode
+sudo pamac build visual-studio-code-bin
+
+# TODO: configure vim
 
